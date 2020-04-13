2e5655c692af8f1ea81c0ec2596f8c5a2a2be310
gabeaster <gab.easter@live.com>
2020-04-13 17:12:45 -0400

    cases add-todos, toggle-todos, clear-todos built; failing to compile. debugging

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import './App.css';
+import { Card } from 'semantic-ui-react';
+
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [state, dispatch] = useReducer(Reducer, initialState);
+
+  const handleChange = e => {
+    setTodo(e.target.value);
+  };
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: "add-todos", payload: todo });
+    setTodo("");
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <h1>Reduce Your Todos</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          onChange={handleChange}
+          placeholder="Enter a todo..."
+        />
+        <button type="submit">Zang!</button>
+      </form>
+
+      <button onClick={() => dispatch({ type: "clear-todos" })}>
+        Clear 'em
+      </button>
+      <div className="card-container">
+        {state.todos.map(todo => (
+          <Card
+            key={todo.id}
+            onClick={() => dispatch({ type: "toggle-todos", payload: todo })}
+          >
+            <p>{todo.item}</p>
+            <p>Done yet? {todo.completed ? "Yep" : "Nope"}</p>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
--- todo/src/Reducers/Reducer.jsx
+++ todo/src/Reducers/Reducer.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 
-const initialState = {
+export const initialState = {
     item: 'Learn about reducers',
     completed: false,
-    id: 3892987589
+    id: Date.now()
 }
 
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case "add-todos":
+            return {
+                ...state,
+                todos: [
+                    ...state.todos,
+                    {item: action.payload,
+                    completed: false,
+                    id: Date.now()
+                    }
+                ]
+            };
+        case "toggle-todos":
+            return {
+                ...state,
+                todos: state.todos.map(todo => {
+                if (todo.id === action.payload.id) {
+                    return { ...todo, completed: !todo.completed };
+                } else {
+                    return todo;
+                }
+                })
+            };
+    case "clear-todos":
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.completed !== true)
+      };
+    default:
+      return state;
+  }
+};
+    
 
-
-
-export default initialState;
